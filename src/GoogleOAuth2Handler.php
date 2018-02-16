0636fb78ce60535e31e1b7eed91d8cea6ae3f35f
Jordan Hall <jordan.hall@rapidweb.biz>
2018-02-16 11:11:50 +0000

    Refactor to use OAuth Handler

---
+++ src/GoogleOAuth2Handler.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace RapidWeb\GooglePeopleAPI;
+
+class GoogleOAuth2Handler
+{
+    private $clientId;
+    private $clientSecret;
+    private $scopes;
+    private $refreshToken;
+    private $client;
+    
+    public $authUrl;
+
+    public function __construct($clientId, $clientSecret, $scopes, $refreshToken = '')
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->scopes = $scopes;
+        $this->refreshToken = $refreshToken;
+
+        $this->setupClient();
+    }
+
+    private function setupClient()
+    {
+        $this->client = new \Google_Client();
+
+        $this->client->setClientId($this->clientId);
+        $this->client->setClientSecret($this->clientSecret);
+        $this->client->setRedirectUri('urn:ietf:wg:oauth:2.0:oob');
+        $this->client->setAccessType('offline');
+        $this->client->setApprovalPrompt('force');
+
+        foreach($this->scopes as $scope)  {
+            $this->client->addScope($scope);
+        }
+
+        if ($this->refreshToken) {
+            $this->client->refreshToken($this->refreshToken);
+        } else {
+            $this->authUrl = $this->client->createAuthUrl();
+        }
+    }
+
+    public function getRefreshToken($authCode)
+    {
+        $this->client->authenticate($authCode);
+        $accessToken = $this->client->getAccessToken();
+        return $accessToken['refresh_token'];
+    }
+
+}
--- src/setup.php
+++ src/setup.php
@@ -1,10 +1,10 @@
 <?php
 
+use RapidWeb\GooglePeopleAPI\GoogleOAuth2Handler;
+
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
 }
-
-$client = new Google_Client();
 
 echo PHP_EOL;
 echo '*** PHP Google People API - Setup ***';
@@ -24,31 +24,22 @@ $clientId = trim(readline('Google Client ID: '));
 $clientSecret = trim(readline('Google Client Secret: '));
 echo PHP_EOL;
 
-$client->setClientId($clientId);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri('urn:ietf:wg:oauth:2.0:oob');
-$client->setAccessType('offline');
-$client->setApprovalPrompt('force');
+$scopes = ['profile', 'https://www.googleapis.com/auth/contacts'];
 
-$client->addScope('profile');
-$client->addScope('https://www.googleapis.com/auth/contacts');
-
-$authUrl = $client->createAuthUrl();
+$googleOAuth2Handler = new GoogleOAuth2Handler($clientId, $clientSecret, $scopes);
 
 echo 'Now, go to the following URL, sign in to your Google Account,';
 echo PHP_EOL;
 echo 'and copy-paste the auth code you receive below.';
 echo PHP_EOL.PHP_EOL;
 
-echo $authUrl;
+echo $googleOAuth2Handler->authUrl;
 echo PHP_EOL.PHP_EOL;
 
 $authCode = trim(readline('Auth Code: '));
 echo PHP_EOL;PHP_EOL;
 
-$client->authenticate($authCode);
-$accessToken = $client->getAccessToken();
-$refreshToken = $accessToken['refresh_token'];
+$refreshToken = $googleOAuth2Handler->getRefreshToken($authCode);
 
 echo 'This account\'s refresh token is: '.$refreshToken;
 echo PHP_EOL.PHP_EOL;
@@ -59,8 +50,6 @@ echo 'access to the Google account.';
 echo PHP_EOL.PHP_EOL;
 
 /*
-$client->refreshToken($accessToken);
-
 $peopleService = new Google_Service_People($client);
 
 $connections = $peopleService->people_connections->listPeopleConnections('people/me', array('personFields' => 'names,emailAddresses'));
