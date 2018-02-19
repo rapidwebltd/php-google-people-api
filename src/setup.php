980f424b06fee61b83ab843bc78e3c49f31733c0
Jordan Hall <jordan.hall@rapidweb.biz>
2018-02-19 15:50:07 +0000

    Remove additional brackets

--- src/setup.php
+++ src/setup.php
@@ -27,7 +27,7 @@ echo PHP_EOL;
 
 $scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/contacts', 'https://www.googleapis.com/auth/contacts.readonly'];
 
-$googleOAuth2Handler = new GoogleOAuth2Handler()($clientId, $clientSecret, $scopes);
+$googleOAuth2Handler = new GoogleOAuth2Handler($clientId, $clientSecret, $scopes);
 
 echo 'Now, go to the following URL, sign in to your Google Account,';
 echo PHP_EOL;
